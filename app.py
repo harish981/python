4a66e01982f5bb9f892f051c05e6fe2ba4a64efb
harish981 <113511497+harish981@users.noreply.github.com>
2024-11-18 18:03:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def home():
     return "<h1>Welcome to Jenkins Tutorials</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')  # Host set to 0.0.0.0 to make the app accessible from any IP
+    app.run(debug=True, host='127.0.0.1')  # Host set to 0.0.0.0 to make the app accessible from any IP
 
